e8ca0ea68043a525c0ce6913cd9022a5454ffecc
Gustavo Borges <gustavohbcn@gmail.com>
2021-09-05 00:20:10 -0300

    continuando a aula

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,11 +1,44 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
+  produtos: Produto[] = [
+    new Produto('LIV001', 'Livro 001', 29.9),
+    new Produto('LIV002', 'Livro 002', 39.9),
+    new Produto('LIV003', 'Livro 003', 29.9),
+  ];
+  @Get()
+  obterTodos(): Produto[] {
+    return this.produtos;
+  }
 
-    @Get()
-    obterTodos(): string {
-        return 'Lista todos os produtos'
-    }
+  @Get(':id')
+  obterUm(@Param() params): Produto {
+    return this.produtos[0];
+  }
 
+  @Post()
+  criar(@Body() produto: Produto) {
+    produto.id = 100;
+    this.produtos.push(produto);
+  }
+
+  @Put()
+  alterar(@Body() produto: Produto): Produto {
+    return produto;
+  }
+
+  @Delete(':id')
+  apagar(@Param() params) {
+    this.produtos.pop();
+  }
 }
